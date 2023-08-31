5a7cb30bdd575731e3c506b0308eeb3f1b30937f
br1_dev <magaro.bruno@gmail.com>
2023-08-31 10:25:26 -0300

    reloj OK & excel v6

--- src/components/Reloj.jsx
+++ src/components/Reloj.jsx
@@ -3,17 +3,20 @@ import '../style/Header.css';
 export const Reloj = ({ estadoScroll }) => {
 	const [time, setTime] = useState();
 	const timeRef = useRef([]);
-
+	//const timeTest = '9:45';
 	useEffect(() => {
 		setInterval(() => {
 			const datosTiempo = new Date();
 			const datosHora = datosTiempo.toLocaleTimeString().substring(0, 5);
+			//console.log(datosTiempo.toLocaleTimeString());
 			setTime(datosHora);
 		}, 1000);
 	}, []);
 	return (
 		<div className="reloj" ref={timeRef}>
-			<div style={{ fontSize: estadoScroll ? 16 : 20 }}>{time}</div>
+			<div style={{ fontSize: estadoScroll ? 16 : 20 }}>
+				{time ? (time.length < 5 ? `0${time}` : time) : ''}
+			</div>
 		</div>
 	);
 };
