12f798ab08f8b547805cab954bbfdbd7dfb60864
br1_dev <magaro.bruno@gmail.com>
2023-10-12 08:28:39 -0300

    test env

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { DATOS_EXCEL } from './DATOS_EXCEL/DATOS_EXCEL.js';
-import { Login } from './components/Login';
+//import { Login } from './components/Login';
 import { Header } from './components/Header';
 import { Seccion } from './components/Seccion';
 import { NavFooter } from './components/NavFooter';
